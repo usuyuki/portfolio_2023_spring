26aacf272388b6ed86c2f57c10a90f63d4563c9f
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-02-17 14:14:33 +0900

    feat fonts設定

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode:'jit',
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class', // mediaはos依存に自動でなるため手動で変えられうようにclassへ
+    theme: {
+        extend: {
+            fontFamily: {
+                serif: ['var(--heading-font)', ...fontFamily.serif],
+                sans: ['var(--body-font)', ...fontFamily.sans],
+            },
+        colors: {
+            white: 'var(--white)',
+            black: 'var(--black)',
+            pink: 'var(--pink)',
+            yellow: 'var(--yellow)',
+            blue: 'var(--blue)',
+            'ui-bg': 'var(--ui-bg)',
+            'kmsn-bg': 'var(--kmsn-bg)',
+        },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
