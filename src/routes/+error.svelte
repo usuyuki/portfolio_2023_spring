3e07c29708aa69d3a64486e0430c49d267a74f96
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-02-18 23:54:11 +0900

    feat エラーページ仮組み

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+<h1 class="text-3xl text-center mx-2 my-2 ">
+    <p class="">{$page.error.message}</p>
+</h1>
+<div class="flex mx-2 my-2 justify-center">
+</div>
