a7a22896a44737e80d98b0f3f681eafc137904f3
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-02-19 00:10:26 +0900

    wip test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		globals: true,
+		environment: 'jsdom'
 	}
 };
 
