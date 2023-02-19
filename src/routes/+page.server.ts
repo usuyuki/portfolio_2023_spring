4e40daf54b06957832dbb0fdba8348566ab85213
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-02-19 17:25:25 +0900

    feat top-level awaitの廃止

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,18 @@
-import database from '../lib/utils/notion';
+import { NOTION_API_KEY } from '$env/static/private';
+import { Client } from '@notionhq/client';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	const notion = new Client({
+		auth: NOTION_API_KEY
+	});
+	const response = await notion.databases.query({
+		database_id: 'b8ec3c117d1b4677947153bbe44bd42d'
+	});
+	let data: { [key: string]: string } = {};
+
+	response.results.forEach((row: any) => {
+		data[row.properties.key.title[0].plain_text] = row.properties.value.rich_text[0].plain_text;
+	});
+	return data;
+}) satisfies PageServerLoad;
