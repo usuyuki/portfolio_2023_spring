80b71a2ef0f7ec46e90a8ff10345ef810577d71c
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-14 23:45:36 +0900

    feat もっとconsoleをおしゃれに、あとアイコン画像追加

--- src/lib/utils/console/portfolioVersionLogger.ts
+++ src/lib/utils/console/portfolioVersionLogger.ts
@@ -1,7 +1,7 @@
 export const portfolioVersionLogger = () => {
 	// console.logの結果をおしゃれに出すスタイル
 	console.log(
-		'%cusuyuki portfolio🌂  %c\n%cversion:6%cPowered By usuyuki',
+		'%cusuyuki portfolio🌂%c\n%cversion:6%cPowered By usuyuki',
 		'background: white; color: gray; border-radius:2px; font-size: 1.1rem;padding: 0.25rem 0.25rem;',
 		'',
 		'background: mediumslateblue; color: white;margin-top:0.5rem; border-radius:2px; padding: 0.25rem 0.25rem;',
