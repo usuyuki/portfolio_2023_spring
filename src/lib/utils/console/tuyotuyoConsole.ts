1b9b0c9c3432f48300da5c27779d764e3629de8b
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-15 11:13:06 +0900

    feat さらなるconsoleのみため改善

--- src/lib/utils/console/tuyotuyoConsole.ts
+++ src/lib/utils/console/tuyotuyoConsole.ts
@@ -2,5 +2,5 @@ export const tuyotuyoConsole = (string: string) => {
 	// console.logの結果をおしゃれに出すスタイル
 	const style =
 		"border-radius:4px;border:2px dotted pink;margin:0.25rem;padding:1.5rem 1rem;font-family:'Kiwi Maru';text-shadow: #FC0 1px 0 10px;";
-	console.log('%c' + string, style);
+	console.log('%cひとこと : ' + string, style);
 };
