9f4d13e163d9d2b1667de6214441793a202695b3
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-15 17:21:38 +0900

    fix コンソールもっとよく

--- src/lib/utils/console/portfolioVersionLogger.ts
+++ src/lib/utils/console/portfolioVersionLogger.ts
@@ -4,7 +4,7 @@ export const portfolioVersionLogger = () => {
 		'%cusuyuki portfolio🌂%c\n%cversion:6%cPowered By usuyuki',
 		'background: white; color: gray; border-radius:2px; font-size: 1.1rem;padding: 0.25rem 0.25rem;',
 		'',
-		'border-left:0.5rem solid pink;background: mediumslateblue; color: white;margin-top:0.5rem; border-radius:2px; padding: 0.25rem 0.25rem;',
-		"border-left:0.5rem solid pink;margin-left:0.25rem;background: aquamarine; color: black;margin-top:0.5rem; border-radius:2px; padding: 0.25rem 0.25rem;font-family:'Kiwi Maru';"
+		'border-left:0.4rem solid pink;background: mediumslateblue; color: white;margin-top:0.5rem; border-radius:2px; padding: 0.25rem 0.25rem;',
+		"border-left:0.4rem solid pink;margin-left:0.25rem;background: aquamarine; color: black;margin-top:0.5rem; border-radius:2px; padding: 0.25rem 0.25rem;font-family:'Kiwi Maru';"
 	);
 };
---
+++ src/lib/utils/console/snsLinkProvider.ts
@@ -0,0 +1,14 @@
+export const snsLinkProvider = () => {
+	// console.logの結果をおしゃれに出すスタイル
+	console.log(
+		'%cTwitter%chttps://twitter.com/usuyuki26%c\n%cMisskey%chttps://m5y.usuyuki.net/@usuyuki%c\n%cGitHub%chttps://github.com/usuyuki',
+		'background: #1da1f2; color: white;padding: 0.25rem 0.25rem;',
+		'background: black; color: white;padding: 0.25rem 0.25rem;',
+		'',
+		'margin-top:4px;background: #739900; color: white;padding: 0.25rem 0.25rem;',
+		'margin-top:4px;background: black; color: white;padding: 0.25rem 0.25rem;',
+		'',
+		'margin-top:4px;background: #7e44c4; color: white;padding: 0.25rem 0.25rem;',
+		'margin-top:4px;background: black; color: white;padding: 0.25rem 0.25rem;',
+	);
+};
