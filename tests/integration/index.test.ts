0530fe9c8f113a19ce78a6340a04cc99fc9a0c71
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-18 16:01:44 +0900

    feat 200チェックのE2Eテスト

--- tests/integration/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('インデックスページのh1に「うすゆきどっとねっと」が出る', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('うすゆきどっとねっと');
-});
