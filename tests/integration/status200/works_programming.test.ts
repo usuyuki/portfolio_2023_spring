f5937559d6b41097b08745ee0041fc76bfe5cdaf
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-19 19:00:57 +0900

    feat 200のテストをファイル分けして並列処理させる

---
+++ tests/integration/status200/works_programming.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('ページが表示されるかのテスト', () => {
+	test('/works/programming', async ({ page }) => {
+		const response = await page.goto('/works/programming');
+		expect(response).not.toBe(null);
+		if (response === null) return; //静的解析を黙らせるために意味がないがnullチェックをしている
+		expect(response.status()).toBe(200);
+	});
+});
