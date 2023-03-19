e6adb20298fa9c48389ad19e8223db99546600e4
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-03-19 12:17:30 +0900

    feat 作品個別で技術情報など出す

---
+++ src/lib/components/atom/text/word/WordForTag.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let title: string;
+	export let url: string;
+</script>
+
+<span class="px-4 py-2">
+	<p class="p-2">{title}</p>
+</span>
--- src/routes/works/programming/[id]/+page.svelte
+++ src/routes/works/programming/[id]/+page.svelte
@@ -2,6 +2,7 @@
 	import PossibleLinkButton from '$lib/components/atom/button/PossibleLinkButton.svelte';
 	import NormalHead from '$lib/components/atom/head/NormalHead.svelte';
 	import SentenceFrame from '$lib/components/atom/text/sentence/SentenceFrame.svelte';
+	import WordForTag from '$lib/components/atom/text/word/WordForTag.svelte';
 	import WordWithEmoji from '$lib/components/atom/text/word/WordWithEmoji.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
@@ -14,39 +15,77 @@
 	ogImage={data.data.gallery[0]}
 />
 
-<div class="flex justify-center items-center md:flex-row flex-col mb-12 md:mb-4">
+<div class="flex justify-center items-center md:flex-row flex-col mb-4 md:mb-4">
 	{#if data.data.logo !== false}
 		<img loading="lazy" src={data.data.logo} class="w-10 h-10 mr-2 object-contain" alt="ロゴ" />
 	{/if}
 
 	<h1 class="text-3xl font-serif">{data.data.name}</h1>
 </div>
+<div class="mb-6 flex justify-center items-center mx-4 md:mx-0">
+	<p class="">
+		{data.data.summary}
+	</p>
+</div>
 
 <div class="flex justify-center md:block">
 	<div class="md:flex justify-center flex-wrap items-stretch mt-2 mb-6">
-		<WordWithEmoji emoji="🚀" emojiMean="ローンチ日" content={data.data.publishedAt} />
-		<WordWithEmoji emoji="🧍" emojiMean="誰のために作った？" content={data.data.toWhom} />
-		<WordWithEmoji emoji="🍽" emojiMean="目的" content={data.data.whatToOffer} />
+		<WordWithEmoji
+			className="mb-4 md:mb-0"
+			emoji="🚀"
+			emojiMean="ローンチ日"
+			content={data.data.publishedAt}
+		/>
+		<WordWithEmoji
+			className="mb-4 md:mb-0"
+			emoji="🙎"
+			emojiMean="開発形態"
+			content={data.data.form.name}
+		/>
+		<WordWithEmoji
+			className="mb-4 md:mb-0"
+			emoji="🍽"
+			emojiMean="ジャンル"
+			content={data.data.genre.name}
+		/>
+		<WordWithEmoji
+			className="mb-4 md:mb-0"
+			emoji="🏹"
+			emojiMean="誰のために作った？"
+			content={data.data.toWhom}
+		/>
 	</div>
 </div>
 
-<div class="flex justify-center items-center flex-row mb-4 mt-8">
+<div class="flex justify-center items-center flex-row mb-20 mt-4">
 	<PossibleLinkButton title="GitHub🐙" link={data.data.gitHub} />
 	<PossibleLinkButton title="サイト🔗" link={data.data.link} />
 </div>
 
+<!-- 使用技術 -->
+<div class="relative shadow-inner rounded-xl mb-12 p-4 w-full">
+	<div class="flex justify-center absolute inset-x-0 -top-4">
+		<h2 class="text-xl text-center font-serif bg-white px-4 rounded-2xl">技術</h2>
+	</div>
+	<div class="flex justify-center flex-wrap items-stretch my-4">
+		{#each data.data.tech as tech}
+			<WordForTag title={tech.name} url={tech.id} />
+		{/each}
+	</div>
+</div>
+
 <div class="flex justify-center flex-wrap items-stretch mt-16 mb-6">
 	<SentenceFrame
-		className="w-full mb-6"
-		title="ひとことで"
-		content={data.data.summary}
+		className="md:w-1/2 w-full mb-6"
+		title="目的"
+		content={data.data.whatToOffer}
 		borderColor="pink"
 	/>
 	<SentenceFrame
 		className="md:w-1/2 w-full mb-6"
 		title="背景"
 		content={data.data.background}
-		borderColor="black"
+		borderColor="blue"
 	/>
 	<SentenceFrame
 		className="md:w-1/2 w-full mb-6"
