9978ae2eab77404e2575222d649b9259eb17a2cc
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-05-02 01:43:07 +0900

    feat 読み込みのウィンドウ追加

---
+++ src/lib/components/atom/loading/PageTransition.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+</script>
+
+<div
+	style="max-width:84vw"
+	class="flex fixed left-6 top-12 z-50 justify-center items-center py-2 px-4 bg-black rounded-lg opacity-95 md:left-12"
+	transition:fly={{ x: -300, duration: 500 }}
+>
+	<div class="flex justify-center">
+		<div
+			class="w-8 h-8 rounded-full border-4 animate-spin border-yellow border-t-transparent"
+		/>
+	</div>
+	<div>
+		<p class="pl-2 text-xl text-white">
+			がんばってページを<br class="md:hidden" />ビルドしています🏗<br />ちょっとまってね🙇
+		</p>
+		<p class="pl-4 text-xs text-gray-400">
+			サーバーレスに無理言ってSSRしてもらっているため、<br
+				class="hidden md:block"
+			/>ページ遷移まで時間を要することがあります
+		</p>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import ScreenFrame from '$lib/components/atom/frame/ScreenFrame.svelte';
+	import PageTransition from '$lib/components/atom/loading/PageTransition.svelte';
 	import Footer from '$lib/components/molecule/footer/Footer.svelte';
 	import LeftMenu from '$lib/components/molecule/menu/LeftMenu.svelte';
+	import { navigating } from '$app/stores';
 	import '../app.css';
 </script>
 
 <ScreenFrame />
+{#if $navigating}
+	<PageTransition />
+{/if}
 <LeftMenu />
 <main>
 	<slot />
