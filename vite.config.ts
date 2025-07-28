b450af713adab3706976d851e0e3215b9cef3f09
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-07-28 23:57:27 +0900

    feat pnpm upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	// @ts-expect-error SvelteKitとviteの噛み合わせが今のバージョンだと合わずにエラーになるので一旦もみ消す https://stackoverflow.com/questions/79297794/sveltekit-no-overload-matches-this-call-error-invite-config-js-when-working-w
 	plugins: [sveltekit()],
 	test: {
 		include: ['tests/unit/**/*.{test,spec}.{js,ts}'],
